1a2bd7b7247acad74ab5101c7f153d0e2b1a8c28
batterteam <batterteam@gmail.com>
2018-12-27 14:26:19 +0100

    New class for recording time played in game

---
+++ MindMase-Project/Assets/Sudoku/Sudoku/Scripts/GameTimeRecord.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameTimeRecord
+{
+
+    public float sudokuTimePlayed = 0;
+    public float crosswordTimePlayed = 0;
+    public float pacmanTimePlayed = 0;
+
+    public static float GameTimeRecorded
+    {
+
+        get
+        {
+            return PlayerPrefs.GetFloat("GameTimeRecorded", 0f);
+        }
+        set
+        {
+            PlayerPrefs.SetFloat("GameTimeRecorded", value);
+        }
+    }
+
+    public static float CrosswordTimeRecorded
+    {
+
+        get
+        {
+            return PlayerPrefs.GetFloat("CrosswordTimeRecorded", 0f);
+        }
+        set
+        {
+            PlayerPrefs.SetFloat("CrosswordTimeRecorded", value);
+        }
+    }
+
+    public static float GameTimeSession
+    {
+        get
+        {
+            return PlayerPrefs.GetFloat("GameTimeSession", 0f);
+        }
+        set
+        {
+            PlayerPrefs.SetFloat("GameTimeSession", value);
+        }
+    }
+
+}
